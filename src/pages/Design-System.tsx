908791144dd1b6937015d817184903c3ec26d424
EricG442 <ericg206@outlook.com>
2026-06-29 21:02:07 -0700

    Merged design-system branch into main to delete design-system branch

--- src/pages/Design-System.tsx
+++ src/pages/Design-System.tsx
@@ -1,25 +1,7 @@
-import { Colors } from "@/design-system/Colors";
-import { Typography } from "@/design-system/Typography";
-import { Buttons } from "@/design-system/Buttons";
-import { Spacing } from "@/design-system/Spacing";
-import { Separator } from "@/components/ui/separator";
-
 export function DesignSystem() {
     return (
         <div>
-            <Colors />
 
-            <Separator className="mt-1" />
-
-            <Typography />
-
-            <Separator className="mt-1" />
-
-            <Buttons />
-
-            <Separator className="mt-1" />
-
-            <Spacing />
         </div>
     );
 }
