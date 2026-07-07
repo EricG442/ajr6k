e706e1854e706fb36443d03ee75b600feb2fd738
EricG442 <ericg206@outlook.com>
2026-07-07 10:36:18 -0700

    Refactor router configuration for improved readability

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -7,16 +7,19 @@ import Article from "@/pages/Article";
 import Editor from "@/pages/Editor";
 import LeaguePage from "@/pages/League";
 
-export const router = createBrowserRouter([
-    {
-        element: <Layout />,
-        children: [
-            { path: "/", element: <Home /> },
-            { path: "/league/:league", element: <LeaguePage /> },
-            { path: "/dashboard", element: <Dashboard /> },
-            { path: "/login", element: <Login />},
-            { path: "/article/:slug", element: <Article /> },
-            { path: "/article/editor", element: <Editor /> },
-        ],
-    },
-]);
+export const router = createBrowserRouter(
+    [
+        {
+            element: <Layout />,
+            children: [
+                { path: "/", element: <Home /> },
+                { path: "/league/:league", element: <LeaguePage /> },
+                { path: "/dashboard", element: <Dashboard /> },
+                { path: "/login", element: <Login />},
+                { path: "/article/:slug", element: <Article /> },
+                { path: "/article/editor", element: <Editor /> },
+            ],
+        },
+    ],
+    { basename: "/ajr6k/" }
+);
